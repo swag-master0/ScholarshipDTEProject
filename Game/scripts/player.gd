d9c242c59cdfa2d31ac4bb1b543552c4eb0d7d6e
OilyMorris <mariogaming132@gmail.com>
2024-04-07 13:07:55 +1200

    Basic movement changes and Blender additions
    peice of shit code

--- Game/scripts/player.gd
+++ Game/scripts/player.gd
@@ -1,8 +1,13 @@
 extends CharacterBody3D
 
 
-const SPEED = 5.0
-const JUMP_VELOCITY = 4.5
+const SPEED = 8
+const JUMP_VELOCITY = 8
+const JUMP_FALLMULTIPLIER = 2
+const TURN_VELOCITY = 10
+
+@onready var character = $PlayerModel
+@onready var pivot = $CameraPivot
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
@@ -11,21 +16,25 @@ var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
 func _physics_process(delta):
 	# Add the gravity.
 	if not is_on_floor():
-		velocity.y -= gravity * delta
+		velocity.y -= gravity * delta * JUMP_FALLMULTIPLIER
 
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var input_dir = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+	var input_dir = Input.get_vector("left", "right", "forward", "backward")
 	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	
+	
 	if direction:
 		velocity.x = direction.x * SPEED
 		velocity.z = direction.z * SPEED
+		character.rotation.y = lerp_angle(character.rotation.y, atan2(-direction.x, -direction.z), delta * TURN_VELOCITY)
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 		velocity.z = move_toward(velocity.z, 0, SPEED)
-
+	
+	
 	move_and_slide()
