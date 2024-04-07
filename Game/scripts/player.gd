79c5467b951ab63eb705d9f963f2226d9422f5ac
OilyMorris <mariogaming132@gmail.com>
2024-04-07 14:43:39 +1200

    Changes to player looking

--- Game/scripts/player.gd
+++ Game/scripts/player.gd
@@ -8,9 +8,18 @@ const TURN_VELOCITY = 10
 
 @onready var character = $PlayerModel
 @onready var pivot = $CameraPivot
+@onready var cursor = $Cursor
 
-# Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
+
+
+func GetMousePosition():
+	var mousePos = get_viewport().get_mouse_position()
+	var camera = get_tree().root.get_camera_3d()
+	
+	var dropPlane = Plane(Vector3(0, 1, 0), character.global_position.y)
+	var position3D = dropPlane.intersects_ray(camera.project_ray_origin(mousePos), camera.project_ray_normal(mousePos))
+	return position3D
 
 
 func _physics_process(delta):
@@ -21,17 +30,17 @@ func _physics_process(delta):
 	# Handle jump.
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
+	
+	cursor.global_position = GetMousePosition()
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
+	#character.rotation.y = lerp_angle(character.rotation.y, atan2(-direction.x, -direction.z), delta * TURN_VELOCITY)
+	character.look_at(Vector3(cursor.position.x, character.position.y + 1, cursor.position.z))
+	
 	var input_dir = Input.get_vector("left", "right", "forward", "backward")
 	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
-	
-	
 	if direction:
 		velocity.x = direction.x * SPEED
 		velocity.z = direction.z * SPEED
-		character.rotation.y = lerp_angle(character.rotation.y, atan2(-direction.x, -direction.z), delta * TURN_VELOCITY)
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 		velocity.z = move_toward(velocity.z, 0, SPEED)
