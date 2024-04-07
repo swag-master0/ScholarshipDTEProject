3358ef5ef7684324761a2e5b5cb84db4b8b7103d
OilyMorris <mariogaming132@gmail.com>
2024-04-07 20:04:06 +1200

    Broke it

--- Game/scripts/player.gd
+++ Game/scripts/player.gd
@@ -32,9 +32,17 @@ func _physics_process(delta):
 		velocity.y = JUMP_VELOCITY
 	
 	cursor.global_position = GetMousePosition()
-
-	#character.rotation.y = lerp_angle(character.rotation.y, atan2(-direction.x, -direction.z), delta * TURN_VELOCITY)
-	character.look_at(Vector3(cursor.position.x, character.position.y + 1, cursor.position.z))
+	
+	#var faceDirection = Vector3.FORWARD
+	var faceDirection = character.get_global_transform().basis.z
+	
+	if Input.is_action_pressed("click"):
+		character.look_at(cursor.position)
+	else:
+		character.rotation.y = lerp_angle(character.rotation.y, atan2(-faceDirection.x, -faceDirection.z), delta * TURN_VELOCITY)
+	
+	
+	
 	
 	var input_dir = Input.get_vector("left", "right", "forward", "backward")
 	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
