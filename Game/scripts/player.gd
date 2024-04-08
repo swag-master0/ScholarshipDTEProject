5f18217a907134405aca0eb9a5ae2784399a8d5f
OilyMorris <mariogaming132@gmail.com>
2024-04-08 17:06:27 +1200

    There is mould growing in my brain after what I did here

--- Game/scripts/player.gd
+++ Game/scripts/player.gd
@@ -2,8 +2,8 @@ extends CharacterBody3D
 
 
 const SPEED = 8
-const JUMP_VELOCITY = 8
-const JUMP_FALLMULTIPLIER = 2
+const JUMP_VELOCITY = 10
+const JUMP_FALLMULTIPLIER = 5
 const TURN_VELOCITY = 10
 
 @onready var character = $PlayerModel
@@ -15,7 +15,7 @@ var faceDirection = Vector3.FORWARD
 
 
 func MousePosition():
-	if  ready: # brainrot code
+	if  ready: # This statement looks very dumb but without it it'll sometimes crash. The crashing isn't even consistant either!
 		var mousePos = get_viewport().get_mouse_position()
 		var camera = get_tree().root.get_camera_3d()
 	
@@ -30,17 +30,16 @@ func _physics_process(delta):
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 	
-	
-	cursor.global_position = MousePosition()
+	if ready: # The fact I have to do this twice is utterly fucking retarded
+		cursor.global_position = MousePosition()
 	
 	if (Input.is_action_pressed("forward") or Input.is_action_pressed("left") or Input.is_action_pressed("right") or Input.is_action_pressed("backward")):
 		faceDirection = Vector3(Input.get_action_strength("right") - Input.get_action_strength("left"), 0 ,Input.get_action_strength("backward") - Input.get_action_strength("forward")).normalized()
 	
 	if Input.is_action_pressed("click"):
 		character.rotation.y = atan2(cursor.position.x, cursor.position.z)
-	elif not Input.is_action_pressed("click"): # this seems stupid, but fuck it
+	elif not Input.is_action_pressed("click"): # This is shitty code, but I fail to care
 		character.rotation.y = lerp_angle(character.rotation.y, atan2(faceDirection.x, faceDirection.z), delta * TURN_VELOCITY)
-	
 	
 	
 	var input_dir = Input.get_vector("left", "right", "forward", "backward")
