5ec00af1cc61f02ae6e4bab79222b3b4259b346e
OilyMorris <mariogaming132@gmail.com>
2024-04-26 20:17:58 +1200

    improved copycats

--- Game/scripts/copycats.gd
+++ Game/scripts/copycats.gd
@@ -13,20 +13,25 @@ extends CharacterBody3D
 @onready var health = info.health
 
 var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
+var direction = Vector3()
 
 
+# INFO:
+# true means the copycat will hide and attack the player when they get near
+# false means they will run away and re-hide from the player
+var behaviorstate = true
 
+
+# main behavior
 func _physics_process(delta):
 	# fall
 	if not is_on_floor():
 		velocity.y -= gravity * delta
 	
 	
-	var direction = Vector3()
-	
+	# hunt player when player is seen
 	for i in self.get_parent_node_3d().get_children():
-		
-		if i is CharacterBody3D and i.is_in_group("player") and global_position.distance_to(i.global_position) < 5:
+		if (i is CharacterBody3D) and (i.is_in_group("player")) and (global_position.distance_to(i.global_position) < 5) and (behaviorstate == true):
 			var player = i
 			
 			raycast.target_position = player.global_position - global_position
@@ -36,11 +41,64 @@ func _physics_process(delta):
 			
 			else:
 				pass
-			
 	
 	direction = nav.get_next_path_position() - global_position
 	direction = direction.normalized()
 	
 	velocity = velocity.lerp(direction * speed, accel * delta)
+	rotation.y = atan2(direction.x, direction.z)
 	
 	move_and_slide()
+
+
+
+
+
+# if game hit player
+func _on_hitbox_body_entered(body):
+	if body.is_in_group("player"):
+		behaviorstate = false
+		$Timer.start()
+		
+		# TODO: should replace with a better way to run away from player
+		nav.target_position = Vector3(randf_range(-500, 500), randf_range(-500, 500), randf_range(-500, 500))
+
+
+# reset to 
+func _on_timer_timeout():
+	behaviorstate = true
+	nav.target_position = Vector3()
+
+
+
+func _on_info_take_damage():
+	behaviorstate = false
+
+
+# replace self with rigidbody upon death
+func _on_info_death():
+	# create a rigid body of itself upon death
+	var rigidbody = $RigidBody3D
+	rigidbody.disable_mode = false
+	
+	var mesh = $MeshInstance3D
+	mesh.reparent(rigidbody)
+	
+	var collision = $CollisionShape3D
+	collision.reparent(rigidbody)
+	
+	rigidbody.reparent(self.get_parent())
+	
+	
+	# delete self upon death
+	self.queue_free()
+	
+
+
+
+
+
+
+
+
+
