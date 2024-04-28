3a3607865f5f9923701c8a993cd9e3ea8e189161
OilyMorris <mariogaming132@gmail.com>
2024-04-28 15:10:29 +1200

    finished copycat level, reworked copycats

--- Game/scripts/copycats.gd
+++ Game/scripts/copycats.gd
@@ -1,99 +1,84 @@
 extends CharacterBody3D
 
-@export var speed = 20
-@export var accel = 10
-@export var damage = 10
+@export var speed : float = 20
 
-@onready var nav : NavigationAgent3D = $NavigationAgent3D
 @onready var raycast = $RayCast3D
+@onready var nav = $NavigationAgent3D
+
+"""
 @onready var mesh = $MeshInstance3D
 @onready var coll = $CollisionShape3D
-
 @onready var info = $Info
-@onready var health = info.health
+"""
 
-var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
+
 var direction = Vector3()
-var player = null
-
-# INFO:
-# true means the copycat will hide and attack the player when they get near
-# false means they will run away and re-hide from the player
-var behaviorstate = true
+var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
 
 
-# main behavior
-func _physics_process(delta):
-	# fall
-	if not is_on_floor():
-		velocity.y -= gravity * delta
-	
-	
-	# set raycast position on the player
-	for i in self.get_parent_node_3d().get_children():
-		if (i is CharacterBody3D) and (i.is_in_group("player")) and (behaviorstate == true):
-			player = i
-			raycast.target_position = player.global_position - global_position
-			
-	
-	
-	# set target position to the player's position if the player is close enough
-	if (raycast.get_collider() == player) and (global_position.distance_to(player.global_position) <= 5):
-		nav.target_position = player.position
-	
-	else:
-		pass
-	
-	direction = nav.get_next_path_position() - global_position
-	direction = direction.normalized()
-	
-
-	velocity = velocity.lerp(direction * speed, accel * delta)
-	mesh.rotation.y = atan2(direction.x, direction.z)
-	coll.rotation.y = atan2(direction.x, direction.z)
-	info.rotation.y = atan2(direction.x, direction.z)
-	
-	
-	move_and_slide()
-	
-	
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
 
 
-
-
-
-# if game hit player
-func _on_hitbox_body_entered(body):
-	if body.is_in_group("player"):
-		behaviorstate = false
-		$Timer.start()
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	if ready:
+		# gravity 
+		if not is_on_floor():
+			velocity.y -= gravity * delta
+		
+		
+		
+		# set raycast target position to the player's position
+		for i in self.get_parent().get_children():
+			if i.is_in_group("player") and i is CharacterBody3D:
+				raycast.target_position = i.position - position
+			#= player.global_position - global_position
+		
+		
+		
+		
+		
+		# player gets close enough for copycat to attack
+		if raycast.get_collider() and raycast.get_collider().is_in_group("player") and raycast.get_collider().global_position.distance_to(self.global_position) < 5 and $Timer.is_stopped():
+			nav.target_position = self.global_position + raycast.target_position
+			$Timer.start()
+			#print("attacking player")
+		
+		# player is within view of copycat
+		elif raycast.get_collider() and raycast.get_collider().is_in_group("player") and raycast.get_collider().global_position.distance_to(self.global_position) > 5:
+			nav.target_position = self.global_position
+			#print("player sighted, but too far to attack")
+		
+		
+		
+		
+		
+		
+		#print_rich("[rainbow]", nav.target_position)
+		$MeshInstance3D2.global_position = nav.target_position
+		
+		direction = nav.get_next_path_position() - global_position
+		direction = direction.normalized()
+		
+		velocity = velocity.lerp(direction * speed, 10 * delta)
 		
 		"""
-		# TODO: should replace with a better way to run away from player
-		var target_position = Vector3(randf_range(-500, 500), randf_range(-500, 500), randf_range(-500, 500))
-		
-		var closest_pos = NavigationServer3D.map_get_closest_point(get_world_3d().get_navigation_map(), target_position)
-		nav.target_position = closest_pos
+		if direction:
+			mesh.rotation.y = atan2(direction.x, direction.z)
+			coll.rotation.y = atan2(direction.x, direction.z)
+			info.rotation.y = atan2(direction.x, direction.z)
 		"""
+		
+		move_and_slide()
 
 
-# reset to 
-func _on_timer_timeout():
-	behaviorstate = true
-	nav.target_position = Vector3()
-
-
-
-func _on_info_take_damage():
-	pass
-
-
-# replace self with rigidbody upon death
 func _on_info_death():
 	print_rich("[rainbow]copycat died lmao")
 	# create a rigid body of itself upon death
 	var rigidbody = $RigidBody3D
-	rigidbody.disable_mode = false
+	rigidbody.set_freeze_enabled(false)
 	
 	var mesh = $MeshInstance3D
 	mesh.reparent(rigidbody)
@@ -106,13 +91,7 @@ func _on_info_death():
 	
 	# delete self upon death
 	self.queue_free()
-	
 
 
-
-
-
-
-
-
-
+func _on_info_take_damage():
+	pass # Replace with function body.
