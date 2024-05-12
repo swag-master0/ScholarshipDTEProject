35faffd04f07d30d9da8e9b0ab8d406b36f8eae7
OilyMorris <109999420+OilyMorris@users.noreply.github.com>
2024-05-12 16:43:41 +1200

    created todo

---
+++ Game/assets/todo.gd
@@ -0,0 +1,16 @@
+#extends Node
+"""
+TODO:
+	
+	create more visual indicators and sound effects to things like:
+		enemy damage
+		player damage
+		grenade and explosive barrel fuse sound/audible cue for when they're about to explode
+	
+	try get playtesting on c1 and get feedback
+	
+	begin progress on c2
+
+
+
+"""
