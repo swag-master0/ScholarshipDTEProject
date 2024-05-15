71e81060d67c2fcb6b2b7acd856c950c88531e27
OilyMorris <mariogaming132@gmail.com>
2024-05-15 13:52:42 +1200

    minor changes

--- Game/assets/todo.gd
+++ Game/assets/todo.gd
@@ -9,10 +9,14 @@ TODO:
 	
 	try get playtesting on c1 and get feedback
 	
-	finish healthwhite (visual flair to health bar)
+	finish healthwhite (visual flair to health bar):
+		DONE
 	
-	begin progress on c2
+	begin progress on c2 (TODO)
 
+	
 
-
+	reference and planning document:
+	https://docs.google.com/document/d/1_fQ-Ee-Z4ZmXX716uf3z78Fp5T5amcVon3N_eA1SpdU/edit
+	
 """
