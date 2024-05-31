19bc0ba6fb84581834afc5f8cc640c2b47df4aa3
OilyMorris <omorris-personal@outlook.com>
2024-05-31 12:20:49 +1200

    volume adjustment settings

--- Game/Elements/function/options_menu/options_menu.gd
+++ Game/Elements/function/options_menu/options_menu.gd
@@ -1,6 +1,8 @@
 extends ColorRect
 
-@onready var windowed_button = $WindowedButton
+@onready var windowed_button = $WindowMode/WindowedButton
+@onready var current_volume = $VolumeSetting/CurrentVolume
+
 
 func _input(event):
 	if event.is_action("escape"):
@@ -19,6 +21,10 @@ func _ready():
 		windowed_button.text = "WINDOWED"
 	elif get_window().mode == 3: #fullscreen
 		windowed_button.text = "FULLSCREEN"
+	
+	AudioServer.get_bus_name(0)
+	
+
 
 func _on_windowed_button_pressed():
 	if get_window().mode == get_window().MODE_WINDOWED:
@@ -29,3 +35,12 @@ func _on_windowed_button_pressed():
 		get_window().mode = get_window().MODE_WINDOWED
 		windowed_button.text = "WINDOWED"
 	
+
+
+
+func _on_increase_volume_pressed():
+	# must test, theres no way this works properly
+	AudioServer.set_bus_volume_db(0, AudioServer.get_bus_volume_db(0) + 5)
+
+func _on_decrease_volume_pressed():
+	AudioServer.set_bus_volume_db(0, AudioServer.get_bus_volume_db(0) - 5)
