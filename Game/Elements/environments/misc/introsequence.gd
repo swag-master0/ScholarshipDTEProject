63b08f1d04cc1539357f7d15132751e70b0b3d46
OilyMorris <omorris-personal@outlook.com>
2024-07-28 13:00:57 +1200

    completed intro sequence

--- Game/Elements/environments/misc/introsequence.gd
+++ Game/Elements/environments/misc/introsequence.gd
@@ -1,13 +1,22 @@
 extends Node
 
 @onready var rail_anims = $"../RailsPlayerHub/AnimationPlayer"
+@onready var rail_carriage = $"../RailsPlayerHub/carraige"
+
 @onready var lights = [
 	$"../Light",
 	$"../Light2"
 ]
-
+@onready var root = $".."
 @onready var player = $"../Player"
 
+@export var object: PackedScene
+
+var activate_movement_tutorial = false
+var activate_jumping_tutorial = false
+var activate_throwing_tutorial = false
+var held = false
+var threw = false
 
 
 
@@ -30,18 +39,58 @@ func _ready():
 
 DEATH")
 	player.dialogue_queue.append("DIAGNOSTIC COURSE IS NOW BEGINNING. ")
-	movement_tutorial()
 	
 
 
+func _process(_delta):
+	if activate_movement_tutorial:
+		if abs(player.velocity.x) > 1 or abs(player.velocity.z) > 1:
+			activate_movement_tutorial = false
+			jumping_tutorial()
+	
+	if activate_jumping_tutorial:
+		if player.velocity.y > 4:
+			activate_jumping_tutorial = false
+			print("jumping tutorial completed")
+			throwing_tutorial()
+	
+	if activate_throwing_tutorial:
+		if player.object:
+			player.SendHintToPlayer("Release MOUSE 1 to throw")
+			held = true
+		
+		if !player.object and held == true:
+			threw = true
+			activate_throwing_tutorial = false
+			print("tutorial complete")
+			tutorial_finished()
+				
+
+
 func movement_tutorial():
+	print("movement tutorial activated")
 	player.dialogue_queue.append("...")
 	player.dialogue_queue.append("...")
 	player.dialogue_queue.append("PLEASE DEMONSTRATE BASIC MOVEMENT CAPABILITIES. ")
 	
-	
-	
 
+func jumping_tutorial():
+	print("jumping tutorial activated")
+	player.dialogue_queue.append("GOOD. ")
+	player.dialogue_queue.append("PLEASE DEMONSTRATE VERTICAL MOVEMENT CAPABILITIES. ")
+
+func throwing_tutorial():
+	print("throwing tutorial activated")
+	player.dialogue_queue.append("GOOD. ")
+	player.dialogue_queue.append("YOU WILL NOW BE PROVIDED WITH A 'cube'")
+	player.dialogue_queue.append("PLEASE DEMONSTRATE PHYSICAL STRENGTH BY HOLDING THE 'cube'")
+
+func tutorial_finished():
+	player.dialogue_queue.append("THANK YOU FOR COMPLETING THE DIAGNOSTIC SEQUENCE. ")
+	player.dialogue_queue.append(0)
+	player.dialogue_queue.append("YEEEES")
+	#insert A-OS monologue here
+	player.dialogue_queue.append("YEEEES")
 
 
 
@@ -50,8 +99,21 @@ func _on_player_dialogue_finished(dialogue):
 		movement_tutorial()
 	elif dialogue == "PLEASE DEMONSTRATE BASIC MOVEMENT CAPABILITIES. ":
 		player.SendHintToPlayer("Use WASD to move")
+		activate_movement_tutorial = true
+	elif dialogue == "PLEASE DEMONSTRATE VERTICAL MOVEMENT CAPABILITIES. ":
+		player.SendHintToPlayer("Use SPACE to jump")
+		activate_jumping_tutorial = true
+	elif dialogue == "YOU WILL NOW BE PROVIDED WITH A 'cube'":
+		var new_object = object.instantiate()
+		root.add_child(new_object)
+		new_object.global_position = Vector3(0, 20, 0)
+		print("added new object")
+	elif dialogue == "PLEASE DEMONSTRATE PHYSICAL STRENGTH BY HOLDING THE 'cube'":
+		player.SendHintToPlayer("Use MOUSE 1 to pick up the cube")
+		activate_throwing_tutorial = true
 	
-	
+	elif dialogue == "YEEEES":
+		rail_anims.play("down")
 	
 	
 
@@ -64,4 +126,7 @@ func _on_crushed_body_entered(body):
 
 
 
-
+func _on_area_3d_body_entered(body):
+	if body.is_in_group("player"):
+		rail_anims.play("up")
+		
