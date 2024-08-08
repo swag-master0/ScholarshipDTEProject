a1dd8fa27a99c917af4e7a916c2fc5c27b4e3d2d
OilyMorris <omorris-personal@outlook.com>
2024-08-08 20:56:01 +1200

    imported improved collision sounds

--- Game/Elements/assets/props/template/prop.gd
+++ Game/Elements/assets/props/template/prop.gd
@@ -1,9 +1,43 @@
 extends RigidBody3D
 
 @export var ungrabbable : bool = false
-@onready var sound = $Sound
+
 
 var ungrabbable_group = "ungrabbable"
+
+@export_category("Audio")
+@export var material : int = 0
+
+# 0
+var generic_sounds = [
+	"res://Art/audio/impact/thump-1.wav",
+	"res://Art/audio/impact/thump-2.wav",
+	"res://Art/audio/impact/thump-3.wav",
+	"res://Art/audio/impact/thump-4.wav"
+]
+
+# 1
+var wood_sounds = [
+	"res://Art/audio/impact/clatter-wood-1.wav",
+	"res://Art/audio/impact/clatter-wood-3.wav"
+]
+
+# 2
+var metal_sounds = [
+	"res://Art/audio/impact/clang-metallic-1.wav",
+	"res://Art/audio/impact/clang-metallic-2.wav"
+]
+
+# 3
+var glass_sounds = [
+	"res://Art/audio/impact/chink_01.wav",
+	"res://Art/audio/impact/chink_02.wav",
+	"res://Art/audio/impact/chink_03.wav",
+	"res://Art/audio/impact/chink_04.wav"
+]
+
+
+@onready var sound = $Sound
 
 
 
@@ -16,6 +50,10 @@ func _ready():
 
 func _on_body_entered(_body):
 	if !sound.is_playing():
+		
+		if material == 0:
+			sound.stream = generic_sounds[0]
+		
 		sound.play()
 
 
