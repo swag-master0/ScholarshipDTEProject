c5f2922879b020c0ad9d9a108c432f8eb74c0fcf
swag-master0 <omorris-personal@outlook.com>
2024-09-19 19:53:34 +1200

    finalised c1_m10
    This level has no real gameplay, and only really exists as an ending to the game

--- Game/Elements/levels/chapter1/c1_m10.gd
+++ Game/Elements/levels/chapter1/c1_m10.gd
@@ -1,11 +1,27 @@
 extends Node3D
 
-
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
+@onready var player = $Player
+@onready var stinger = $Stinger
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+func _ready():
+	await get_tree().create_timer(3).timeout
+	player.dialogue_queue.append(1)
+	player.dialogue_queue.append("i know what it looks like, but bare with me for a sec")
+	player.dialogue_queue.append(8)
+	player.dialogue_queue.append("i've had to leave it here because i'm scared of it...")
+	player.dialogue_queue.append(2)
+	player.dialogue_queue.append("i don't know if it's armed, and i fear it could be the end of us")
+	
+
+
+func _on_animation_player_animation_finished(anim_name):
+	print(anim_name)
+	if anim_name == "up":
+		stinger.play()
+
+
+func _on_trigger_body_entered(body):
+	if body.is_in_group("player"):
+		pass
+		# dialogue here
